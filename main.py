badb93bcadf597e9ab25cc0fad1b0998eeedc952
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-04-27 20:27:57 +0100

    added more patterns

--- main.py
+++ main.py
@@ -37,6 +37,36 @@ print ("===============")
 
 for i in range(3,0,-1):
     for j in range(i):
-        print (" "*j, end = "")
+        print (" "*i, end = "")
 
-    print ("* "* i)
+    print (" *"* i)
+
+"""
+     *
+   *  *
+  *  *  *
+ *  *  *  *
+"""
+print ("====================================")
+for i in range (4,0,-1):
+    for j in range (i):
+        print 
+
+
+
+
+'''
+    *
+  * *
+* * *
+'''
+n = 4
+for i in range(n, 0, -1):
+    for j in range(1, i):
+        print(" ", end = " ")
+    print("* "*(n-i), end="") 
+    if (n-i == 0):
+        print("*", end="")
+    for j in range(i, 0, -1):
+        print("", end = "")
+    print("* "*(n-i))
