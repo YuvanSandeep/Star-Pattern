ab04e4a4c27c1981b574ae5de6658c508d27fbba
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-05-11 20:09:16 +0100

    finally worked out how to fix the pyramid

--- main.py
+++ main.py
@@ -70,3 +70,8 @@ for i in range(n, 0, -1):
     for j in range(i, 0, -1):
         print("", end = "")
     print("* "*(n-i))
+print ("=============================================")
+n = 4
+for i in range (1, n+1):
+    print ((n-i)*" ", end = "")
+    print (i*("* "))
