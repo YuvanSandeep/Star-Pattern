5f0ba93e4cc60b5c836f90848ac89c3aae892edd
Yuvan Sandeep <yuvanapsandeep@gmail.com>
2026-05-11 20:18:52 +0100

    upside down pattern pyramid

--- main.py
+++ main.py
@@ -75,3 +75,8 @@ n = 4
 for i in range (1, n+1):
     print ((n-i)*" ", end = "")
     print (i*("* "))
+print ("======================================")
+n = 83
+for i in range (n-1,0,-1):
+    print ((n-i)*" ", end = "")
+    print (i*("* "))
